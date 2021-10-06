5ad1729c81fde7c5c76c466731a2eec3531daf94
aiceru <aiceru@gmail.com>
2021-10-06 16:49:29 +0900

    Initial Commit

---
+++ MetalOnSwiftUI/MetalOnSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  MetalOnSwiftUIApp.swift
+//  MetalOnSwiftUI
+//
+//  Created by Wooseok Son on 2021/10/06.
+//
+
+import SwiftUI
+
+@main
+struct MetalOnSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
